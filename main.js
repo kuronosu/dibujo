ebd72417cc66dd792de34dde79785032c4f11d4f
Andres Felipe Martinez Salazar <andresfelipe.2031@gmail.com>
2022-06-28 09:55:00 -0500

    Cuerpo

--- main.js
+++ main.js
@@ -18,3 +18,30 @@ ctx.arc(190, 45, 3, 0, Math.PI * 2, true);
 ctx.fill();
 ctx.arc(210, 45, 3, 0, Math.PI * 2, true);
 ctx.fill();
+
+
+ctx.beginPath();
+ctx.moveTo(200, 80);
+ctx.lineTo(200, 180);
+ctx.strokeStyle = "red";
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "#ff0000"
+ctx.moveTo(200, 110);
+ctx.lineTo(130, 110);
+ctx.lineTo(120, 120);
+ctx.moveTo(200, 110);
+ctx.lineTo(280, 110);
+ctx.lineTo(290, 100);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "navy";
+ctx.moveTo(200, 180);
+ctx.lineTo(150, 280);
+ctx.lineTo(130, 280);
+ctx.moveTo(200, 180);
+ctx.lineTo(250, 280);
+ctx.lineTo(270, 280);
+ctx.stroke();
